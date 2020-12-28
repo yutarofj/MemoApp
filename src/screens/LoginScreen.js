44b90ddc0609589385196b2f45c9fba0dbc0b866
AzureAD\藤江　悠太郎 <fujie.yutaro@headwaters.co.jp>
2020-12-28 09:55:07 +0900

    Signup and Login with firebase

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
+import firebase from "firebase";
 
 class LoginScreen extends React.Component {
     state = {
@@ -8,7 +9,14 @@ class LoginScreen extends React.Component {
     }
 
     handleSubmit() {
-
+        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then((result) => {
+                console.log('success!', result.user);
+                this.props.navigation.navigate('Home');
+            })
+            .catch((error) => {
+                console.log(error);
+            })
         // this.props.navigation.navigate('Home');
 
         // Login
